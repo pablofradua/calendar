18ed002e0ee8664f5061ff172575afd743840a95
Pablo Fradua <pablo.fradua@gmail.com>
2022-04-21 22:57:50 +0200

    Days printed and select/unselect event captured

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
+import Day from "./components/Day.vue";
+const logEvent = (dayStatus) =>{
+  console.log(`${dayStatus.dayOfMonth} has been ${dayStatus.isSelected?'selected':'unselected'}`)
+}
 </script>
-
 <template>
-
-  <main>
-    <h3>alive</h3>
-  </main>
+  <Day
+    v-for="n in 31"
+    :key="n"
+    :dayOfMonth="n"
+    @onselect="(dayStatus) =>logEvent(dayStatus)"
+  />
 </template>
-
-<style>
-</style>
---
+++ src/components/Day.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { ref, computed } from "vue";
+const props = defineProps({
+  dayOfMonth: Number,
+});
+const emit = defineEmits(["onselect"]);
+const selected = ref(false);
+const toggleDay = () => {
+  selected.value = !selected.value;
+  emit('onselect', {dayOfMonth: props.dayOfMonth, isSelected: selected.value})
+};
+const selectedClass = computed(() => {
+  return selected.value ? "selected" : "";
+});
+</script>
+<template>
+  <span :class="selectedClass" @click="toggleDay">{{ dayOfMonth }}</span>
+</template>
+
+<style>
+span {
+  background-color: var(--color-primary-30);
+  padding: 16px;
+  color: white;
+}
+span.selected {
+  background-color: var(--color-primary-40);
+}
+</style>
